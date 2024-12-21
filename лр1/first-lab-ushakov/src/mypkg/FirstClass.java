2a3b5e964fcfb1e1de52dc112c5f0c2f55c182b3
Lanancist <148477601+Lanancist@users.noreply.github.com>
2024-12-22 01:09:45 +0300

    Add files via upload

---
+++ лр1/first-lab-ushakov/src/mypkg/FirstClass.java
@@ -0,0 +1,12 @@
+package mypkg;
+
+import mypkg2.MyText;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        MyText myText = new MyText();
+
+        System.out.println(myText.getMyText());
+    }
+}
---
+++ лр1/first-lab-ushakov/src/mypkg2/MyText.java
@@ -0,0 +1,14 @@
+package mypkg2;
+
+public class MyText {
+
+    private String myText = "Hello World =)";
+
+    public String getMyText() {
+        return myText;
+    }
+
+    public void setMyText(String myText) {
+        this.myText = myText;
+    }
+}
